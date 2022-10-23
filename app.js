36f363a3202ebe866d79c8d1f7e5ae2511bcb0fe
Luiz Gabriel <91032465+osgabrielluiz@users.noreply.github.com>
2022-10-23 16:13:12 -0300

    add

---
+++ app.js
@@ -0,0 +1,43 @@
+// alert("Olá Mundo");
+// var nome = prompt("Qual seu nome?");
+// alert("Bem vindo! "+ nome);
+
+// var num1 = parseInt(prompt("Digite um número: "));
+// var num2 = parseInt(prompt("Digite outro número: "));
+// var soma = num1 + num2;
+// alert("Resultado: "+soma);
+
+// prompt("Digite um número: ");
+
+// var paraInt = prompt("Digite um valor: ");
+// alert("Valor em inteiro: "+paraInt);
+
+// var n1 = parseInt(prompt("Digite um número: "));
+// var n2 = parseInt(prompt("Digite outro número: "));
+// var sub = n1 - n2;
+// alert("A subtração é: "+sub);
+
+// var n1 = parseInt(prompt("Digite um número: "));
+// var n2 = parseInt(prompt("Digite outro número: "));
+// var soma = n1 + n2;
+// alert("A soma é: "+soma);
+
+// var n1 = parseInt(prompt("Digite um número: "));
+// var n2 = parseInt(prompt("Digite outro número: "));
+// var div = n1 / n2;
+// alert("A divisão é: "+div);
+
+// var nota1 = parseInt(prompt("Digite a primeira nota: "));
+// var nota2 = parseInt(prompt("Digite a segunda nota: "));
+// var media = (nota1 + nota2) / 2;
+// alert("A media é "+media);
+
+// var nota1 = parseInt(prompt("Digite a primeira nota: "));
+// var nota2 = parseInt(prompt("Digite a segunda nota: "));
+// var resto = nota1 % nota2;
+// alert("O resto da divisão é "+resto);
+
+var nota1 = parseInt(prompt("Digite a primeira nota: "));
+var nota2 = parseInt(prompt("Digite a segunda nota: "));
+var elev = nota1 ** nota2;
+alert(""+nota1 "elevado à "+nota2 "é igual a: "+elev);
